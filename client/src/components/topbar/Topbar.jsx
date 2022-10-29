9a9048a841af1c66296f89e85db5fca788a40f38
Abdullah <abdullahrecon999@gmail.com>
2022-10-29 14:02:04 +0500

    Chnaged image and added pm2 as production manager

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar() {
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">lamaadmin</span>
+          <span className="logo">Admin Dashboard</span>
         </div>
         <div className="topRight">
           <div className="topbarIconContainer">
@@ -21,7 +21,7 @@ export default function Topbar() {
           <div className="topbarIconContainer">
             <Settings />
           </div>
-          <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
+          <img src="https://w7.pngwing.com/pngs/81/570/png-transparent-profile-logo-computer-icons-user-user-blue-heroes-logo-thumbnail.png" alt="" className="topAvatar" />
         </div>
       </div>
     </div>
